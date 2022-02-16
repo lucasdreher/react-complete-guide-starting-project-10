ff155ec77a7195f81c6a37cf79d1b813808628e2
Lucas Dreher <lucasdreher@gmail.com>
2022-02-16 11:17:42 +0100

    Testing User Interaction & State

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Greeting = () => {
+	const [ changedText, setChangedText ] = useState(false);
+
+	const changeTextHandle = () => {
+		setChangedText(true);
+	};
+
 	return (
 		<div>
 			<h2>Hello World!</h2>
-			<p>It's good to see you!</p>
+			{!changedText && <p>It's good to see you!</p>}
+			{changedText && <p>Changed!</p>}
+			<button onClick={changeTextHandle}>Change Text</button>
 		</div>
 	);
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
@@ -12,5 +13,44 @@ describe('Greeting component', () => {
 		//Assert
 		const helloWorldElement = screen.getByText('Hello World!');
 		expect(helloWorldElement).toBeInTheDocument();
+	});
+
+	test('renders "good to see you" as a text if the "Change Text" button was NOT clicked', () => {
+		// Arrange
+		render(<Greeting />);
+
+		//Act
+		//...nothing
+
+		//Assert
+		const textElement = screen.getByText('good to see you', { exact: false });
+		expect(textElement).toBeInTheDocument();
+	});
+
+	test('renders "Changed!" if the "Change Text" button was clicked', () => {
+		// Arrange
+		render(<Greeting />);
+
+		//Act
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		//Assert
+		const textElement = screen.getByText('Changed!');
+		expect(textElement).toBeInTheDocument();
+	});
+
+	test('does NOT render "good to see you" if the "Change Text" button was clicked', () => {
+		// Arrange
+		render(<Greeting />);
+
+		//Act
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		//Assert
+		const textElement = screen.queryByText('good to see you', { exact: false });
+		expect(textElement).toBeNull();
+		// expect(() => screen.getByText('good to see you', { exact: false })).toThrow();
 	});
 });
